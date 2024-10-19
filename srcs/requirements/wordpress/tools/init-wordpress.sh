f4639f9948f7ba51d08babebcc7869df23207692
Woorim Kim <wrkimlimi@gmail.com>
2024-10-19 16:49:00 +0900

    fix: wait until mariadb is ready

--- srcs/requirements/wordpress/tools/init-wordpress.sh
+++ srcs/requirements/wordpress/tools/init-wordpress.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-    cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-    sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" /var/www/html/wp-config.php
-    sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wp-config.php
-    sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wp-config.php
-    sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wp-config.php
+until mysql -h"$WORDPRESS_DB_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1"; do
+  echo "Waiting for MariaDB to be ready..."
+  sleep 2
+done
+
+cd /var/www/html/wordpress/
+
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+    wp config create \
+    --allow-root \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=$WORDPRESS_DB_HOST \
+    --path=/var/www/html/wordpress \
+    --skip-check
 fi
 
+# wordpress 설치
+wp core install \
+    --allow-root \
+    --url=$DOMAIN \
+    --title=$title \
+    --admin_user=$WORDPRESS_ADMIN_USER \
+    --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+    --admin_email=$WORDPRESS_ADMIN_EMAIL \
+    --locale=ko_KR
+
+# 사용자 생성
+wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
+
+# 포그라운드로 php-fpm 실행
 exec php-fpm7.4 -F
